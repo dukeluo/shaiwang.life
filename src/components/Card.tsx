d5e733742632151e8054d9cf83b01d0473b432fb
Huan Luo <dukeluo@outlook.com>
2024-06-26 23:05:17 +0800

    refactor: update tailwind typography settings

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,7 @@ const CardRoot = ({
 const CardLink = ({ children, ...props }: React.PropsWithChildren<LinkProps>) => {
   return (
     <>
-      <span className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
+      <span className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50" />
       <Link {...props}>
         <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>
--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -1,10 +1,5 @@
-import clsx from 'clsx'
-import { PropsWithChildren } from 'react'
+import { ReactNode } from 'react'
 
-type Props = PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>
-
-export const Prose = ({ children, className }: Props) => {
-  return (
-    <div className={clsx(className, 'md:prose-md prose prose-zinc w-full dark:prose-dark xl:prose-lg')}>{children}</div>
-  )
+export const Prose = ({ children, className }: { children: ReactNode; className: string }) => {
+  return <div className={`${className}  prose w-full md:prose-lg lg:prose-xl dark:prose-invert`}>{children}</div>
 }
