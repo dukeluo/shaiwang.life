0051e3d22f1966bcc53e87d1d211578604786a2c
Huan Luo <dukeluo@outlook.com>
2024-07-06 21:59:34 +0800

    feat: add header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,183 +1,61 @@
-import { useRouter } from 'next/router'
-import React, { useLayoutEffect, useRef } from 'react'
+import clsx from 'clsx'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
-import { Avatar } from './Avatar'
-import { AvatarContainer } from './AvatarContainer'
 import { Container } from './Container'
 import { DesktopNavigation, MobileNavigation } from './Navigation'
 import { ThemeToggle } from './ThemeToggle'
 
-const clamp = (number: number, a: number, b: number) => {
-  const min = Math.min(a, b)
-  const max = Math.max(a, b)
-  return Math.min(Math.max(number, min), max)
+const AvatarContainer = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      className={clsx(
+        className,
+        'size-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10'
+      )}
+      {...props}
+    />
+  )
+}
+
+const Avatar = () => {
+  return (
+    <Link href="/" aria-label="Home" className="pointer-events-auto">
+      <Image
+        src=""
+        alt=""
+        sizes="2.25rem"
+        className="size-9 rounded-full bg-zinc-100 object-cover dark:bg-zinc-800"
+        priority
+      />
+    </Link>
+  )
 }
 
 export const Header = () => {
-  const isHomePage = useRouter().pathname === '/'
-
-  const headerRef = useRef<HTMLDivElement | null>(null)
-  const avatarRef = useRef<HTMLDivElement | null>(null)
-  const isInitial = useRef(true)
-
-  useLayoutEffect(() => {
-    const downDelay = avatarRef.current?.offsetTop ?? 0
-    const upDelay = 64
-
-    function setProperty(property: any, value: any) {
-      document.documentElement.style.setProperty(property, value)
-    }
-
-    function removeProperty(property: any) {
-      document.documentElement.style.removeProperty(property)
-    }
-
-    function updateHeaderStyles() {
-      const { top, height } = headerRef.current!.getBoundingClientRect()
-      const scrollY = clamp(window.scrollY, 0, document.body.scrollHeight - window.innerHeight)
-
-      if (isInitial.current) {
-        setProperty('--header-position', 'sticky')
-      }
-
-      setProperty('--content-offset', `${downDelay}px`)
-
-      if (isInitial.current || scrollY < downDelay) {
-        setProperty('--header-height', `${downDelay + height}px`)
-        setProperty('--header-mb', `${-downDelay}px`)
-      } else if (top + height < -upDelay) {
-        const offset = Math.max(height, scrollY - upDelay)
-        setProperty('--header-height', `${offset}px`)
-        setProperty('--header-mb', `${height - offset}px`)
-      } else if (top === 0) {
-        setProperty('--header-height', `${scrollY + height}px`)
-        setProperty('--header-mb', `${-scrollY}px`)
-      }
-
-      if (top === 0 && scrollY > 0 && scrollY >= downDelay) {
-        setProperty('--header-inner-position', 'fixed')
-        removeProperty('--header-top')
-        removeProperty('--avatar-top')
-      } else {
-        removeProperty('--header-inner-position')
-        setProperty('--header-top', '0px')
-        setProperty('--avatar-top', '0px')
-      }
-    }
-
-    function updateAvatarStyles() {
-      if (!isHomePage) {
-        return
-      }
-
-      const fromScale = 1
-      const toScale = 36 / 64
-      const fromX = 0
-      const toX = 2 / 16
-
-      const scrollY = downDelay - window.scrollY
-
-      let scale = (scrollY * (fromScale - toScale)) / downDelay + toScale
-      scale = clamp(scale, fromScale, toScale)
-
-      let x = (scrollY * (fromX - toX)) / downDelay + toX
-      x = clamp(x, fromX, toX)
-
-      setProperty('--avatar-image-transform', `translate3d(${x}rem, 0, 0) scale(${scale})`)
-
-      const borderScale = 1 / (toScale / scale)
-      const borderX = (-toX + x) * borderScale
-      const borderTransform = `translate3d(${borderX}rem, 0, 0) scale(${borderScale})`
-
-      setProperty('--avatar-border-transform', borderTransform)
-      setProperty('--avatar-border-opacity', scale === toScale ? 1 : 0)
-    }
-
-    function updateStyles() {
-      updateHeaderStyles()
-      updateAvatarStyles()
-      isInitial.current = false
-    }
-
-    updateStyles()
-    window.addEventListener('scroll', updateStyles, { passive: true })
-    window.addEventListener('resize', updateStyles)
-
-    return () => {
-      window.removeEventListener('scroll', updateStyles, { passive: true } as any) // TODO: why is TS complaining here?
-      window.removeEventListener('resize', updateStyles)
-    }
-  }, [isHomePage])
-
   return (
-    <>
-      <header
-        className="pointer-events-none relative z-50 flex flex-col"
-        style={{
-          height: 'var(--header-height)',
-          marginBottom: 'var(--header-mb)',
-        }}
-      >
-        {isHomePage && (
-          <>
-            <div ref={avatarRef} className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]" />
-            <Container
-              className="top-0 order-last -mb-3 pt-3"
-              style={{ position: 'var(--header-position)' } as unknown as React.CSSProperties}
-            >
-              <div
-                className="top-[var(--avatar-top,theme(spacing.3))] w-full"
-                style={{ position: 'var(--header-inner-position)' } as unknown as React.CSSProperties}
-              >
-                <div className="relative">
-                  <AvatarContainer
-                    className="absolute left-0 top-3 origin-left transition-opacity"
-                    style={{
-                      opacity: 'var(--avatar-border-opacity, 0)',
-                      transform: 'var(--avatar-border-transform)',
-                    }}
-                  />
-                  <Avatar
-                    large
-                    className="block size-16 origin-left"
-                    style={{ transform: 'var(--avatar-image-transform)' }}
-                  />
-                </div>
-              </div>
-            </Container>
-          </>
-        )}
-        <div
-          ref={headerRef}
-          className="top-0 z-10 h-16 pt-6"
-          style={{ position: 'var(--header-position)' } as unknown as React.CSSProperties}
-        >
-          <Container
-            className="top-[var(--header-top,theme(spacing.6))] w-full"
-            style={{ position: 'var(--header-inner-position)' } as unknown as React.CSSProperties}
-          >
-            <div className="relative flex gap-4">
-              <div className="flex flex-1">
-                {!isHomePage && (
-                  <AvatarContainer>
-                    <Avatar />
-                  </AvatarContainer>
-                )}
-              </div>
-              <div className="flex flex-1 justify-end md:justify-center">
-                <MobileNavigation className="pointer-events-auto md:hidden" />
-                <DesktopNavigation className="pointer-events-auto hidden md:block" />
-              </div>
-              <div className="flex justify-end md:flex-1">
-                <div className="pointer-events-auto">
-                  <ThemeToggle />
-                </div>
+    <header className="pointer-events-none relative z-50 flex flex-col">
+      <div className="top-0 z-10 h-16 pt-6">
+        <Container className="w-full">
+          <div className="relative flex gap-4">
+            <div className="flex flex-1">
+              <AvatarContainer>
+                <Avatar />
+              </AvatarContainer>
+            </div>
+            <div className="flex flex-1 justify-end md:justify-center">
+              <MobileNavigation className="pointer-events-auto md:hidden" />
+              <DesktopNavigation className="pointer-events-auto hidden md:block" />
+            </div>
+            <div className="flex justify-end md:flex-1">
+              <div className="pointer-events-auto">
+                <ThemeToggle />
               </div>
             </div>
-          </Container>
-        </div>
-      </header>
-      {isHomePage && <div style={{ height: 'var(--content-offset)' }} />}
-    </>
+          </div>
+        </Container>
+      </div>
+    </header>
   )
 }
