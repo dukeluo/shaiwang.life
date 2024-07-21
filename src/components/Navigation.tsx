2c6f071bf4c4a83a8a321090bef871820aefe9d7
Huan Luo <dukeluo@outlook.com>
2024-07-21 12:07:24 +0800

    feat: update navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,9 @@
-import { Popover, Transition } from '@headlessui/react'
+import { Popover, PopoverBackdrop, PopoverButton, PopoverPanel, Transition, TransitionChild } from '@headlessui/react'
 import { RiCloseLine, RiMenuLine } from '@remixicon/react'
 import clsx from 'clsx'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { Fragment } from 'react'
+import { Fragment, HTMLAttributes, PropsWithChildren } from 'react'
 
 export enum Route {
   Home = '/',
@@ -41,7 +41,7 @@ export const NavigationItems = [
   },
 ] as const
 
-export const NavLink = ({ href, children }: React.PropsWithChildren<{ href: string }>) => {
+export const NavLink = ({ href, children }: PropsWithChildren<{ href: string }>) => {
   return (
     <Link href={href} className="transition hover:text-primary">
       {children}
@@ -49,7 +49,7 @@ export const NavLink = ({ href, children }: React.PropsWithChildren<{ href: stri
   )
 }
 
-const NavItem = ({ href, children }: React.PropsWithChildren<{ href: string }>) => {
+const NavItem = ({ href, children }: PropsWithChildren<{ href: string }>) => {
   const isActive = useRouter().pathname === href
 
   return (
@@ -64,17 +64,7 @@ const NavItem = ({ href, children }: React.PropsWithChildren<{ href: string }>) 
   )
 }
 
-export const MobileNavItem = ({ href, children }: React.PropsWithChildren<{ href: string }>) => {
-  return (
-    <li>
-      <Popover.Button as={Link} href={href} className="block py-2">
-        {children}
-      </Popover.Button>
-    </li>
-  )
-}
-
-export const DesktopNavigation = (props: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) => {
+export const DesktopNavigation = (props: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) => {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
@@ -98,15 +88,24 @@ export const DesktopNavigation = (props: React.PropsWithChildren<React.HTMLAttri
   )
 }
 
-export const MobileNavigation = (props: React.HTMLAttributes<HTMLDivElement>) => {
+const MobileNavItem = ({ href, children }: PropsWithChildren<{ href: string }>) => {
+  return (
+    <li>
+      <PopoverButton as={Link} href={href} className="block py-2">
+        {children}
+      </PopoverButton>
+    </li>
+  )
+}
+
+export const MobileNavigation = (props: HTMLAttributes<HTMLDivElement>) => {
   return (
     <Popover {...props}>
-      <Popover.Button className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
-        Menu
-        <RiMenuLine className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" />
-      </Popover.Button>
-      <Transition.Root>
-        <Transition.Child
+      <PopoverButton className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 outline-none ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
+        <RiMenuLine />
+      </PopoverButton>
+      <Transition>
+        <TransitionChild
           as={Fragment}
           enter="duration-150 ease-out"
           enterFrom="opacity-0"
@@ -115,9 +114,9 @@ export const MobileNavigation = (props: React.HTMLAttributes<HTMLDivElement>) =>
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Popover.Overlay className="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80" />
-        </Transition.Child>
-        <Transition.Child
+          <PopoverBackdrop className="fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80" />
+        </TransitionChild>
+        <TransitionChild
           as={Fragment}
           enter="duration-150 ease-out"
           enterFrom="opacity-0 scale-95"
@@ -126,15 +125,15 @@ export const MobileNavigation = (props: React.HTMLAttributes<HTMLDivElement>) =>
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          <Popover.Panel
+          <PopoverPanel
             focus
             className="fixed inset-x-4 top-8 z-50 origin-top rounded-3xl bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800"
           >
             <div className="flex flex-row-reverse items-center justify-between">
-              <Popover.Button aria-label="Close menu" className="-m-1 p-1">
+              <PopoverButton aria-label="Close menu" className="-m-1 p-1 outline-none">
                 <RiCloseLine className="size-6 text-zinc-500 dark:text-zinc-400" />
-              </Popover.Button>
-              <h2 className="text-sm font-medium">Navigation</h2>
+              </PopoverButton>
+              <h2 className="text-sm font-medium">Menu</h2>
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
@@ -145,9 +144,9 @@ export const MobileNavigation = (props: React.HTMLAttributes<HTMLDivElement>) =>
                 ))}
               </ul>
             </nav>
-          </Popover.Panel>
-        </Transition.Child>
-      </Transition.Root>
+          </PopoverPanel>
+        </TransitionChild>
+      </Transition>
     </Popover>
   )
 }
