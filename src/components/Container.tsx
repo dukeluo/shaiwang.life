f109e4b9c5f43810733693aab97f7bc697c0c23c
Huan Luo <dukeluo@outlook.com>
2024-07-27 17:42:14 +0800

    refactor: update Container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,40 +1,22 @@
 import clsx from 'clsx'
-import { forwardRef } from 'react'
+import { HTMLAttributes } from 'react'
 
-const OuterContainer = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(function OuterContainer(
-  { className, children, ...props },
-  ref
-) {
-  return (
-    <div ref={ref} className={clsx('sm:px-8', className)} {...props}>
-      <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
-    </div>
-  )
-})
+const OuterContainer = ({ className, children, ...props }: HTMLAttributes<HTMLDivElement>) => (
+  <div className={clsx('sm:px-8', className)} {...props}>
+    <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+  </div>
+)
 
-const InnerContainer = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(function InnerContainer(
-  { className, children, ...props },
-  ref
-) {
-  return (
-    <div ref={ref} className={clsx('relative px-4 sm:px-8 lg:px-12', className)} {...props}>
-      <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
-    </div>
-  )
-})
+const InnerContainer = ({ className, children, ...props }: HTMLAttributes<HTMLDivElement>) => (
+  <div className={clsx('relative px-4 sm:px-8 lg:px-12', className)} {...props}>
+    <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+  </div>
+)
 
-const ContainerRoot = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(function Container(
-  { children, ...props },
-  ref
-) {
+export const Container = ({ children, ...props }: HTMLAttributes<HTMLDivElement>) => {
   return (
-    <OuterContainer ref={ref} {...props}>
+    <OuterContainer {...props}>
       <InnerContainer>{children}</InnerContainer>
     </OuterContainer>
   )
-})
-
-export const Container = Object.assign(ContainerRoot, {
-  Outer: OuterContainer,
-  Inner: InnerContainer,
-})
+}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,24 +6,22 @@ import { NavigationItems, NavLink } from './Navigation'
 export const Footer = () => {
   return (
     <footer className="mt-8">
-      <Container.Outer>
+      <Container>
         <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
-          <Container.Inner>
-            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-              <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                {NavigationItems.map((item) => (
-                  <NavLink key={item.href} href={item.href}>
-                    {item.name}
-                  </NavLink>
-                ))}
-              </div>
-              <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} {process.env.NEXT_PUBLIC_SITE_AUTHOR}. All rights reserved.
-              </p>
+          <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+              {NavigationItems.map((item) => (
+                <NavLink key={item.href} href={item.href}>
+                  {item.name}
+                </NavLink>
+              ))}
             </div>
-          </Container.Inner>
+            <p className="text-sm text-zinc-400 dark:text-zinc-500">
+              &copy; {new Date().getFullYear()} {process.env.NEXT_PUBLIC_SITE_AUTHOR}. All rights reserved.
+            </p>
+          </div>
         </div>
-      </Container.Outer>
+      </Container>
     </footer>
   )
 }
