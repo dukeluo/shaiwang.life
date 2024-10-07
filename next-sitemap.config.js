56d5f629a15f45d509a37721c9283b29b70d48c9
Huan Luo <dukeluo@outlook.com>
2024-10-07 23:18:37 +0800

    chore: update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
-  autoLastmod: false,
-  generateIndexSitemap: false,
-  changefreq: null,
-  priority: null,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-  },
 }
