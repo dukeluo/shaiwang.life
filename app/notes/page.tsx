7bbd1b2017886d7ad7a94fc46b57b8def6814715
Huan Luo <dukeluo@outlook.com>
2024-10-08 23:13:13 +0800

    feat: update notes page layout

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -25,15 +25,15 @@ export default async function Notes() {
           />
         </div>
       </Combobox>
-      <div className="mt-6 flex max-w-xl flex-wrap gap-1 font-mono">
+      <div className="flex flex-wrap gap-1 px-4 font-mono">
         {tags.map((tag) => (
           <Badge key={tag} href={`/tags/${tag}`}>
             #{tag}
           </Badge>
         ))}
       </div>
-      <div className="mt-12 md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-        <div className="flex max-w-3xl flex-col space-y-16">
+      <div className="mt-10 md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+        <div className="flex max-w-3xl flex-col space-y-10">
           {notes.map((note) => (
             <NotePostPreview key={note.slug} post={note} />
           ))}
