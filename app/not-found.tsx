605eddabbc3e06712e4405f4cad8838c670bb825
Huan Luo <dukeluo@outlook.com>
2025-01-15 23:22:20 +0800

    feat: should link to legacy site

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { PageTitle } from './components/PageTitle'
 
 export default function NotFound() {
+  const pathname = usePathname()
+  const isLegacyPost = /^\/\d{4}\/\d{2}\/\d{2}\/.*$/.test(pathname)
+
   return (
     <div className="flex min-h-full flex-col pb-12 pt-16">
       <main className="mx-auto flex w-full max-w-7xl grow flex-col justify-center px-4 sm:px-6 lg:px-8">
@@ -13,9 +19,19 @@ export default function NotFound() {
               Sorry, we couldn&apos;t find the page you&apos;re looking for.
             </p>
             <div className="mt-6">
-              <Link href="/" className="text-base font-medium hover:text-primary">
-                Go back home
-              </Link>
+              {isLegacyPost ? (
+                <Link
+                  href={`${process.env.NEXT_PUBLIC_LEGACY_SITE_URL}${pathname}`}
+                  className="text-base font-medium hover:text-primary"
+                  target="_blank"
+                >
+                  Go to legacy site to read this post
+                </Link>
+              ) : (
+                <Link href="/" className="text-base font-medium hover:text-primary">
+                  Go back home
+                </Link>
+              )}
             </div>
           </div>
         </div>
